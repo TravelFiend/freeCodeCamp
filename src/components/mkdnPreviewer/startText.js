a10140a1e8e50119fd2466eac9529e4830188d77
TravelFiend <mikegrace274@gmail.com>
2021-01-29 02:23:05 -0500

    starting markdown written

---
+++ src/components/mkdnPreviewer/startText.js
@@ -0,0 +1,27 @@
+const startText = `# Sample this Funky Preview
+## Preview some markdown
+
+![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "GitHub Markdown Logo")
+
+
+[My Website]:
+https://mikegrace.tech
+
+[My Website]
+
+Strong emphasis, aka bold, with **asterisks** or __underscores__.
+
+Inline \`<div>code</div>\` has \`back-ticks around\` it.
+
+\`\`\`
+<div>
+  <p>Sizzlin' Sample</p>
+</div>
+\`\`\`
+- list item 1
+- list item 2
+
+> This is a very long line that will still be quoted properly when it wraps. Oh boy let's keep writing to make sure this is long enough to actually wrap for everyone. Oh, you can *put* **Markdown** into a blockquote.
+`;
+
+export default startText;
