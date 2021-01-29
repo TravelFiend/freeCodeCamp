34a55714a87012bb5671b1eebca72d26487388fc
TravelFiend <mikegrace274@gmail.com>
2021-01-29 00:52:27 -0500

    header in place

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import RandomQuotes from './components/randomQuotes/RandomQuotes';
+import Header from './components/header/Header';
 
 const App = () => (
   <Router>
+    <Header />
     <Switch>
       <Route exact path="/" component={RandomQuotes} />
     </Switch>
