553e171d0c1f8a36ad33d645b54d5f32d42691c1
TravelFiend <mikegrace274@gmail.com>
2021-01-31 16:29:01 -0500

    timeChanger component built out

---
+++ src/components/pomodoroClock/TimeChangers.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+class TimeChangers extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { breakOrWork, topText, setTime, time } = this.props;
+    return (
+      <section>
+        <h3 id={`${breakOrWork}-label`}>{topText}</h3>
+        <div className='info'>
+          <button className='buttons' onClick={setTime} id={`${breakOrWork}-decrement`} value='down'>DOWN</button>
+          <p id={`${breakOrWork}-length`}>{time}</p>
+          <button className='buttons' onClick={setTime} id={`${breakOrWork}-increment`} value='up'>UP</button>
+        </div>
+      </section>
+    );
+  }
+}
+
+TimeChangers.propTypes = {
+  breakOrWork: PropTypes.string.isRequired,
+  topText: PropTypes.string.isRequired,
+  setTime: PropTypes.func.isRequired,
+  time: PropTypes.number.isRequired
+};
+
+export default TimeChangers;
